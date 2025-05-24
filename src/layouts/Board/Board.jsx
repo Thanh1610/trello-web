3b9106e05a4da9e330eaa160bcb8c0697361510a
Thanh1610 <Tatuanthanh2105nd@gmail.com>
2025-05-24 12:44:02 +0700

    Axios

--- src/layouts/Board/Board.jsx
+++ src/layouts/Board/Board.jsx
@@ -3,13 +3,25 @@ import AppBar from '~/components/AppBar/AppBar';
 import BoardBar from '~/layouts/components/BoardBar/BoardBar';
 import BoardContent from '~/layouts/components/BoardContent/BoardContent';
 import { mockData } from '~/Apis/mock-data';
+import { useEffect, useState } from 'react';
+import { fetchBoardDetailsAPI } from '~/Apis';
 
 function Board() {
+    const [board, setBoard] = useState(null);
+
+    useEffect(() => {
+        const boardId = '68304dc666a2c82d219d0d5d'
+        fetchBoardDetailsAPI(boardId)
+            .then(res=> {
+                setBoard(res)
+            })
+            
+    })
     return (
         <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
             <AppBar />
-            <BoardBar board={mockData?.board} />
-            <BoardContent board={mockData?.board} />
+            <BoardBar board={board} />
+            <BoardContent board={board} />
         </Container>
     );
 }
